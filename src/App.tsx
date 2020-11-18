6c79f2f51f68e489ae86d7f231f96953017b276b
Jimmy Soussan <jimmy.soussan@ynov.com>
2020-11-18 15:16:16 +0100

    React Router working

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navigation from "./components/Navigation/index";
+import DiscoverPage from "./components/DiscoverPage/index";
+import IndexPage from "./components/IndexPage/index";
+import * as ROUTES from "./constants/routes";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navigation />
+
+        <hr />
+
+        <Route exact path={ROUTES.INDEX} component={IndexPage} />
+        <Route path={ROUTES.DISCOVER} component={DiscoverPage} />
+      </div>
+    </Router>
   );
 }
 
