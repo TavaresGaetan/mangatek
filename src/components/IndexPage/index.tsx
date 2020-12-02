22a2a52d8e61526b4a03ae9dcafef075c3aa9274
jilink <jimmyfac1@gmail.com>
2020-12-02 14:50:11 +0100

    Access to home only if connected (as an example)

--- src/components/IndexPage/index.tsx
+++ src/components/IndexPage/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { withAuthorization } from "../Session";
 
 const IndexPage = () => {
   return <div>INDEX</div>;
 };
 
-export default IndexPage;
+const condition = (authUser: any) => !!authUser;
+export default withAuthorization(condition)(IndexPage);
