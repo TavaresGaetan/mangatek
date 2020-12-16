c9ec76071c346a94b1f70fe08dec4987c44a63fe
Ndeye Awa Diop <nadiop97@gmail.com>
2020-12-16 14:44:52 +0100

    Selling html done

--- src/components/MarketPage/SellingPage/index.tsx
+++ src/components/MarketPage/SellingPage/index.tsx
@@ -6,8 +6,8 @@ const SellingPage = () => {
     libelle:"", 
     prix:0,
     quantite:0,
-    /*categorie,*/
-    /*etat,*/
+    categorie:"",   
+    etat:"",
     auteur:"",
     studio:"",
     collection:"",
@@ -31,23 +31,23 @@ const SellingPage = () => {
           <div >
             <h2>Article à Vendre</h2>
             <form onSubmit={handleSubmit}>
-              <label htmlFor="image">Choisir une image</label> : <input type="file" onChange={handleChange} value={sellingData['image']} name="image" id="image"/><br/><br/> 
+              <label htmlFor="image">Choisir une image</label> : <input type="file" onChange={handleChange}  name="image" id="image"/><br/><br/> 
               <label htmlFor="libelle">Titre/Libellé</label> : <input type="text" onChange={handleChange} value={sellingData['libelle']} name="libelle" id="libelle" placeholder="ex : Nom de l'article"/><br/><br/> 
               <label htmlFor="prix">Prix</label> : <input type="number" onChange={handleChange} value={sellingData['prix']} name="prix" id="prix" placeholder="ex : DVD, Livre, figurine"/> <br/><br/>
               <label htmlFor="quantite">Quantité</label> : <input type="number" onChange={handleChange} value={sellingData['quantite']} name="quantite" id="quantite"/><br/>
               <p>
                 Sélectionner la catégorie : <br/>
-                <input type="checkbox" name="dvd" id="dvd"/><label htmlFor="categorie">DVD</label><br/>
-                <input type="checkbox" name="livre" id="livre"/><label htmlFor="livre">Livre</label><br/>
-                <input type="checkbox" name="figurine" id="figurine"/><label htmlFor="figurine">Figurine</label><br/>
-                <input type="checkbox" name="goodies" id="goodies"/><label htmlFor="goodies">Goodies</label><br/>
+                <input type="radio" name="categorie" id="dvd" onChange={handleChange} value="dvd" /><label htmlFor="dvd">DVD</label><br/>
+                <input type="radio" name="categorie" id="livre" onChange={handleChange} value="livre"/><label htmlFor="livre">Livre</label><br/>
+                <input type="radio" name="categorie" id="figurine" onChange={handleChange} value="figurine"/><label htmlFor="figurine">Figurine</label><br/>
+                <input type="radio" name="categorie" id="goodies" onChange={handleChange} value="goodies"/><label htmlFor="goodies">Goodies</label><br/>
               </p>
               <p>
                 Sélectionner l'état de l'article : <br/>
-                <input type="radio" name="neuf" id="neuf"/><label htmlFor="neuf">neuf</label><br/>
-                <input type="radio" name="tresbonetat" id="tresbonetat"/><label htmlFor="tresbonetat">très bon état</label><br/>
-                <input type="radio" name="bonetat" id="bonetat"/><label htmlFor="bonetat">bon état</label><br/>
-                <input type="radio" name="etatmoyen" id="etatmoyen"/><label htmlFor="etatmoyen">état moyen</label><br/>
+                <input type="radio" name="etat" id="neuf" onChange={handleChange} value="neuf"/><label htmlFor="neuf">neuf</label><br/>
+                <input type="radio" name="etat" id="tresbonetat" onChange={handleChange} value="tresbonetat"/><label htmlFor="tresbonetat">très bon état</label><br/>
+                <input type="radio" name="etat" id="bonetat" onChange={handleChange} value="bonetat"/><label htmlFor="bonetat">bon état</label><br/>
+                <input type="radio" name="etat" id="etatmoyen" onChange={handleChange} value="etatmoyen"/><label htmlFor="etatmoyen">état moyen</label><br/>
               </p>
               <label htmlFor="auteur">Auteur</label> : <input type="text" onChange={handleChange} value={sellingData['auteur']} name="auteur" id="auteur" placeholder="ex : Robert Jones"/><br/><br/>
               <label htmlFor="studio">Studio</label> : <input type="text" onChange={handleChange} value={sellingData['studio']} name="studio" id="studio" placeholder="ex : Disney Studio"/><br/><br/>
