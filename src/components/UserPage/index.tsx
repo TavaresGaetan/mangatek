6b51d9e0651042b717e3e3d2e8839c09c3b4a965
TavaresGaetan <gaetan.tavares@hotmail.fr>
2021-01-13 10:12:43 +0100

    DisplayUserDetails

--- src/components/UserPage/index.tsx
+++ src/components/UserPage/index.tsx
@@ -1,25 +1,68 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { type } from "os";
+import React, {Component} from "react";
+import { Link, withRouter } from "react-router-dom";
 import * as ROUTES from "../../constants/routes";
+import { FirebaseContext } from "../Firebase";
+import { withFirebase } from "../Firebase";
 
 const UserPage = () => {
 
   return (
   <div>
-  <div>User PAGE</div>
-  <Link to ={ROUTES.WATCHLIST}>
-  <button>
-        Watchlist
-  </button>
-  </Link>
-  <Link to ={ROUTES.BOOKSHELF}>
-  <button>
-        Bookshelf
-  </button>
-  </Link>
+  <ProfileBase />
   </div>
   );
 
 };
 
+const INITIAL_STATE = {
+      username: "",
+      email: "",
+      error: null,
+};
+
+type ProfileState = {
+      username?: string;
+      email?: string;
+};
+
+
+class ProfileDetails extends Component<any, ProfileState> {
+      
+      constructor(props: any) {
+        super(props);
+
+        this.state = { username:'', email:''};
+        this.props.firebase.auth.onAuthStateChanged((user: any) => {
+            if (user) {
+              this.props.firebase.getUserData(user.uid, this.changeUserName);
+            }
+      })
+      }
+
+      changeUserName = (user: any) => {
+            console.log(user)
+            this.setState({username: user.username, email: user.email})
+      } 
+      
+      render() {
+            return <div>{this.state.username}
+            <div>{this.state.email}</div>
+            <div>User PAGE</div>
+            <Link to ={ROUTES.WATCHLIST}>
+            <button>
+                  Watchlist
+            </button>
+            </Link>
+            <Link to ={ROUTES.BOOKSHELF}>
+            <button>
+            Bookshelf
+            </button>
+            </Link>
+            </div>
+            ;
+      }
+}
+
+const ProfileBase = withRouter(withFirebase(ProfileDetails));
 export default UserPage;
