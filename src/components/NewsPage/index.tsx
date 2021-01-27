3425f914ca41a8cd03ee82d02628bfeaf2646dee
jilink <jimmyfac1@gmail.com>
2021-01-27 10:24:50 +0100

    create article button

--- src/components/NewsPage/index.tsx
+++ src/components/NewsPage/index.tsx
@@ -1,13 +1,25 @@
 import React from "react";
+import { withFirebase } from "../Firebase";
+import { withRouter } from "react-router-dom";
+
 import Calendar from "./Calendar";
 
-const NewsPage = () => {
+const NewsPage = (props: any) => {
+  const [user, setUser] = React.useState({ admin: false });
+  React.useEffect(() => {
+    props.firebase.auth.onAuthStateChanged((user: any) => {
+      if (user) {
+        props.firebase.getUserData(user.uid, setUser);
+      }
+    });
+  }, []);
   return (
     <div>
+      {user.admin ? <button>Créer un article</button> : null}
       news
       <Calendar />
     </div>
   );
 };
 
-export default NewsPage;
+export default withRouter(withFirebase(NewsPage));
