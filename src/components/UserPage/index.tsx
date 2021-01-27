b5f33d71d3b63baf26797bbe7871682f35452c88
TavaresGaetan <gaetan.tavares@hotmail.fr>
2021-01-27 15:06:21 +0100

    Profile pic

--- src/components/UserPage/index.tsx
+++ src/components/UserPage/index.tsx
@@ -1,5 +1,5 @@
 import { type } from "os";
-import React, {Component} from "react";
+import React, {Component, useState} from "react";
 import { Link, withRouter } from "react-router-dom";
 import * as ROUTES from "../../constants/routes";
 import { FirebaseContext } from "../Firebase";
@@ -16,38 +16,76 @@ const UserPage = () => {
 };
 
 const INITIAL_STATE = {
-      username: "",
-      email: "",
+      username:"",
+      email:"",
+      avatar:"",
       error: null,
 };
 
 type ProfileState = {
       username?: string;
       email?: string;
+      avatar?: any;
+      url?: string;
+      id?: string;
 };
-
 
 class ProfileDetails extends Component<any, ProfileState> {
       
       constructor(props: any) {
         super(props);
 
-        this.state = { username:'', email:''};
+        this.state = { username:'', email:'', avatar:'', url:'',id:''};
         this.props.firebase.auth.onAuthStateChanged((user: any) => {
             if (user) {
               this.props.firebase.getUserData(user.uid, this.changeUserName);
+              this.setState({id: user.uid})
             }
       })
       }
 
+      handleChange = e => {
+            if (e.target.files[0]) {
+                  this.setState({ avatar: e.target.files[0]});
+            }
+
+            console.log("avatar: ", this.state.avatar);
+      };
+
+      handleUpload = (storage) =>{
+            const { avatar } = this.state;
+            const uploadTask = storage.ref(`avatars/${this.state.avatar.name}`).put(this.state.avatar);
+            uploadTask.on(
+               "state_changed",
+               snapshot => {},
+               error => {
+                  console.log(error);
+               },
+               () => {
+                     storage
+                        .ref("avatars")
+                        .child(this.state.avatar.name)
+                        .getDownloadURL()
+                        .then(url => {
+                              this.setState({ url })
+                              this.props.firebase.user(this.state.id).update({url})
+                        });
+               }   
+            );
+      };
+
       changeUserName = (user: any) => {
             console.log(user)
-            this.setState({username: user.username, email: user.email})
+            this.setState({username: user.username, email: user.email, url: user.url})
       } 
       
       render() {
-            return <div>{this.state.username}
-            <div>{this.state.email}</div>
+            return <div>
+            <input type="file" onChange={this.handleChange} />
+            <button onClick={() => {this.handleUpload(this.props.firebase.storage)}}>Upload</button>
+            <img src={this.state.url } height="300" width="400"/>
+            <div>{this.state.username}</div>
+            <div>{this.state.email}</div>         
             <div>User PAGE</div>
             <Link to ={ROUTES.WATCHLIST}>
             <button>
