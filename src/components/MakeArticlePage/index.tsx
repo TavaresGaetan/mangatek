7ff57756c771a49271af3bee9ce974bb821ce65c
jilink <jimmyfac1@gmail.com>
2021-01-27 11:03:27 +0100

    is admin authorization

--- src/components/MakeArticlePage/index.tsx
+++ src/components/MakeArticlePage/index.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { withFirebase } from "../Firebase";
+import { withRouter } from "react-router-dom";
+import { withAdminAuthorization } from "../Session";
 
-const MakeArticlePage = () => {
+const MakeArticlePage = (props: any) => {
+  const [Buser, setUser] = React.useState({ admin: false });
+  React.useEffect(() => {
+    props.firebase.auth.onAuthStateChanged((user: any) => {
+      if (user) {
+        props.firebase.getUserData(user.uid, setUser);
+      }
+    });
+  }, []);
   return <div>make article page</div>;
 };
 
-export default MakeArticlePage;
+const condition = (authUser: any) => !!authUser;
+export default withRouter(
+  withFirebase(withAdminAuthorization(condition)(MakeArticlePage))
+);
