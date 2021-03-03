05fb9c88f91bf9e80ddfe1e2e7048c0cb26cb3ba
Ndeye Awa Diop <nadiop97@gmail.com>
2021-03-03 09:53:57 +0100

    chat updated

--- src/components/MarketPage/ChatPage/index.js
+++ src/components/MarketPage/ChatPage/index.js
@@ -92,7 +92,7 @@ function ChatMessage(props) {
 
   return(<>
     <div className={`message ${messageClass}`}>
-      <img src={photoURL}/>
+      <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png' }/>
       <p>{text}</p>
     </div>
   </>  ) 
